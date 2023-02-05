250bc0b86547dc133f76c10cbfc17f2b74b286ea
Victor-Onyejide <victor.onyejide@gmail.com>
2023-02-04 20:04:39 -0500

    login and register features

---
+++ server/controllers/user.js
@@ -0,0 +1,76 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+
+const register = async (req, res) => {
+    try {
+        const userExists = await User.findOne({ email: req.body.email });
+        if (userExists) {
+            return res.status(200).json({ message: "User already exists", success: false });
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
+        // create new user
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.json({
+            message: "user created successfully",
+            success: true,
+            user: newUser
+        })
+    }
+    catch (error) {
+        res.status(500).send({
+            message: error.message,
+            data: error,
+            success: false,
+        });
+    }
+}
+
+const login = async (req, res) => {
+
+    try {
+        // check if user exists
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(200).json({ message: "user does not exist", success: false });
+        }
+
+        //check password
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword) {
+            return res.json({ message: "Invalid password", success: false })
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "id" }
+        );
+
+        res.json({
+            message: "User logged in successfully",
+            success: true,
+            data: token,
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: error.message,
+            data: error,
+            success: false,
+        });
+    }
+
+}
+
+module.exports = {
+    register, login
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const  router = express.Router();
 
 const { postContent, getAllContent, getQuestion, editPost, deletePost, findQuestion } = require('../controllers/index');
-
+const {register} = require('../controllers/user')
 router.route('/post').post(postContent);
 router.route('/allposts').get(getAllContent);
 router.route('/:id').get(getQuestion).patch(editPost).delete(deletePost);
 router.route('/allposts/search').post(findQuestion);
+
+router.route('/register').post(register);
 
 module.exports = router; 
